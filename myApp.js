2698691e47a21145e7e922006d6f79bca734485b
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-12 11:22:24 +0000

    2022年12月12日 - 01

--- myApp.js
+++ myApp.js
@@ -1,14 +1,59 @@
 require('dotenv').config();
 
+// データベース情報
+const mongoose = require('mongoose');
+const myUser = process.env['username']
+const myPass = process.env['password']
+const myClus = process.env['cluster']
+const myURI = `mongodb+srv://${myUser}:${myPass}@${myClus}/?retryWrites=true&w=majority`
 
-let Person;
+// データベース接続
+mongoose.connect(myURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('Database connection successed')
+})
+.catch(err => {
+  console.error('Database connection failed')
+})
 
+// スキーマ定義
+let personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+// モデル作成
+let Person = mongoose.model('Person', personSchema)
+
+// インスタンス作成とドキュメント保存
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  let Makoto = new Person({
+    name: 'Makoto',
+    age: 67,
+    favoriteFoods: ['Pizza', 'Pasta'],
+  });
+  Makoto.save((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
+// 多数のインスタンス作成とドキュメント保存
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      done(null, data);
+    }
+  })
 };
 
 const findPeopleByName = (personName, done) => {
